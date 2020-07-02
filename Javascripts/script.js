b9a079c5ac7fce86df0fa9573d03cd47b5d83545
amt122109 <62560447+amt122109@users.noreply.github.com>
2020-07-02 14:25:43 -0400

    Create script.js

---
+++ Javascripts/script.js
@@ -0,0 +1,122 @@
+
+<head>
+<title>
+Contact Form
+</title>
+<!-- Meta Tags -->
+<meta charset="utf-8">
+<meta name="generator" content="Wufoo">
+<meta name="robots" content="index, follow">
+
+<!-- JavaScript -->
+<script src="scripts/wufoo.js"></script>
+
+<body id="public">
+<div id="container" class="ltr">
+
+<h1 id="logo">
+<a href="http://www.wufoo.com" title="Powered by Wufoo">Wufoo</a>
+</h1>
+
+  
+<form
+  id="form1"
+  name="form1"
+  class="wufoo topLabel page"
+  accept-charset="UTF-8"
+  autocomplete="off"
+  enctype="multipart/form-data"
+  method="post"
+  novalidate
+      action="https://amt122109.wufoo.com/forms/zcl1i9d16g2nxk/#public"
+  >
+
+<header id="header" class="info">
+<h2 class="">Contact Form</h2>
+<div class=""></div>
+</header><ul>
+<li id="foli1"
+class="notranslate      "><label class="desc" id="title1" for="Field1">
+Message
+<span id="req_1" class="req">*</span>
+</label>
+
+<div>
+<textarea id="Field1"
+name="Field1"
+class="field textarea medium"
+spellcheck="true"
+rows="10" cols="50"
+tabindex="0"
+onkeyup=""
+placeholder=""
+      required  ></textarea>
+
+</div>
+
+<p class="instruct" id="instruct1">
+<small>This field is required.</small>
+</p></li>
+<li id="foli2" class="notranslate      ">
+<label class="desc" id="title2" for="Field2">
+Email Address
+</label>
+<div>
+<input id="Field2" name="Field2" type="email" spellcheck="false" class="field text large" value="" maxlength="255" tabindex="0"       placeholder="" />
+</div>
+<p class="instruct" id="instruct2">
+<small>We won't share this with strangers.</small>
+</p>
+</li>
+<li id="foli5" class="phone notranslate      ">
+<label class="desc" id="title5">
+Phone Number
+</label>
+<span>
+<input id="Field5" name="Field5"       type="tel" class="field text" value="" size="3" maxlength="3" tabindex="0"       onkeyup="autotab(this, document.getElementById('Field5-1'), this); filterNonnumeric(this);"             onfocus="this.selectionStart = this.selectionEnd = this.value.length;" placeholder=""       />
+<label for="Field5">###</label>
+</span>
+<span class="symbol">-</span>
+<span>
+<input id="Field5-1" name="Field5-1"       type="tel" class="field text" value="" size="3" maxlength="3" tabindex="0"       onkeyup="autotab(this, document.getElementById('Field5-2'), document.getElementById('Field5')); filterNonnumeric(this);"       onfocus="this.selectionStart = this.selectionEnd = this.value.length;" placeholder="" />
+<label for="Field5-1">###</label>
+</span>
+<span class="symbol">-</span>
+<span>
+ <input id="Field5-2" name="Field5-2"       type="tel" class="field text" value="" size="4" maxlength="4" tabindex="0"       onkeyup="autotab(this, this, document.getElementById('Field5-1')); filterNonnumeric(this);"             onfocus="this.selectionStart = this.selectionEnd = this.value.length;" placeholder="" />
+<label for="Field5-2">####</label>
+</span>
+</li> <li class="buttons ">
+<div>
+    
+                        <input id="saveForm" name="saveForm" class="btTxt submit"
+    type="submit" value="Submit"
+ />    
+<a href="https://master.wufoo.com/forms/m7p0x3/def/field1=amt122109.wufoo.com/forms/contact-form/"
+target="_blank"
+style="font-size:85% !important;visibility:visible !important;display:inline !important;
+background:none !important;border:none !important;color:blue !important;
+text-indent:0 !important;text-decoration:underline !important;position:static;
+margin:0 !important;padding:0 !important;overflow:none !important
+width:auto !important;height:auto !important;">Report Abuse</a>
+</div>
+</li>
+
+<li class="hide">
+<label for="comment">Do Not Fill This Out</label>
+<textarea name="comment" id="comment" rows="1" cols="1"></textarea>
+<input type="hidden" id="idstamp" name="idstamp" value="4ZNghT7AjoOayOrE+pyuM5Ki/uNYtAXGl1qGzVZJ52o=" />
+<input type="hidden" id="encryptedPassword" name="encryptedPassword" value="" />
+</li>
+</ul>
+</form>
+ 
+
+</div><!--container-->
+
+<a class="powertiny" href="http://www.wufoo.com/" title="Powered by Wufoo"
+style="display:block !important;visibility:visible !important;text-indent:0 !important;position:relative !important;height:auto !important;width:95px !important;overflow:visible !important;text-decoration:none;cursor:pointer !important;margin:0 auto !important">
+<span style="background:url(./images/powerlogo.png) no-repeat center 7px; margin:0 auto;display:inline-block !important;visibility:visible !important;text-indent:-9000px !important;position:static !important;overflow: auto !important;width:62px !important;height:30px !important">Wufoo</span>
+<b style="display:block !important;visibility:visible !important;text-indent:0 !important;position:static !important;height:auto !important;width:auto !important;overflow: auto !important;font-weight:normal;font-size:9px;color:#777;padding:0 0 0 3px;">Designed</b>
+</a></body>
+
